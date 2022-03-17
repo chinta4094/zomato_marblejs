e94989ed37809171fde4277e8c7c9e64bfe63923
sribhaskar <sribhaskar4094@gmail.com>
2022-03-17 11:54:19 +0530

    seventh commit

--- controllers/user/createUser.ts
+++ controllers/user/createUser.ts
@@ -1,31 +1,38 @@
 import { HttpStatus } from '@marblejs/http';
+import { findAncestor } from 'typescript';
 import sendmail from '../../email/email'
+import tokenSchema from '../../schemas/tokenSchema';
 import userSchema from '../../schemas/userSchema';
 
 type userObj = { firstName : string, lastName : string, userName : string, email : string, password : string }
 
 const postUser = async (body : userObj) => {
     try{
-        const findByUsername = await userSchema.find({ "userName" : `${body.userName}`})
-        if(findByUsername.length != 0){
-          return {
-            status : HttpStatus.BAD_REQUEST,
-            message : `userName With ${body.userName} is Already Created !`
-          }
-        }else{
-          var createUser = await userSchema.create(body)
-          sendmail(body.userName,body.email)
-          if(createUser){
+        const findUser = await tokenSchema.find({})
+        if(findUser.length == 0){
+          const findByUsername = await userSchema.find({ "userName" : `${body.userName}`})
+          if(findByUsername.length != 0){
             return {
-              status : HttpStatus.OK,
-              message : `Created A User ${body.userName}`
+              status : HttpStatus.BAD_REQUEST,
+              message : `userName With ${body.userName} is Already Created !`
             }
           }else{
-            return{
-              status : HttpStatus.BAD_REQUEST,
-              message : `User Not Created, Pls Try Again`
+            var createUser = await userSchema.create(body)
+            sendmail(body.userName,body.email)
+            if(createUser){
+              return {
+                status : HttpStatus.OK,
+                message : `Created A User ${body.userName}`
+              }
+            }else{
+              return{
+                status : HttpStatus.BAD_REQUEST,
+                message : `User Not Created, Pls Try Again`
+              }
             }
           }
+        }else{
+          return `To Create Account, You Need To Logout First ...`
         }
       }catch(error){
         return error
